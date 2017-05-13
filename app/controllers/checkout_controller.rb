2dbdb72e16a9fe916476d42de7147e612533fa1b
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2017-05-13 11:16:09 +0300

    Change from session.card to session.order.card

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -28,7 +28,6 @@ class CheckoutController < ApplicationController
   end
 
   def submit_delivery
-    # order_from_params
     @order = OrderForm.from_params(session[:order])
     @order.shipment_id = params[:shipment_id]
     session[:shipment] = params[:shipment_price]
@@ -41,8 +40,9 @@ class CheckoutController < ApplicationController
   def payment
     order_from_session
     redirect_to action: 'delivery' if @order.shipment_id.nil?
-    if session[:card]
-      @card = CreditCardForm.from_params(session[:card])
+    card = session[:order]['card']
+    if card
+      @card = CreditCardForm.from_params(card)
       @card.valid?
     else
       @card = CreditCardForm.new
@@ -52,15 +52,16 @@ class CheckoutController < ApplicationController
   def submit_payment
     @card = CreditCardForm.from_params(params)
     @card.number.delete!('-')
-    session[:card] = @card
+    session[:order]['card'] = @card
     redirect_to action: @card.valid? ? 'confirm' : 'payment'
   end
 
   def confirm
-    redirect_to action: 'payment' unless session[:card]
+    card = session[:order]['card']
+    redirect_to action: 'payment' unless card
     order_from_session
     @order_items = order_items_from_cart
-    @card = CreditCardForm.from_params(session[:card])
+    @card = CreditCardForm.from_params(card)
     @shipment = Shipment.find(@order.shipment_id)
   end
 
@@ -79,18 +80,10 @@ class CheckoutController < ApplicationController
     @order.valid?
   end
 
-  def order_from_params
-    # @order = OrderForm.from_params(params)
-    # depends on the step, cannot use the same logic
-    @order = OrderForm.from_params(session[:order]
-      .merge!(params.permit!.to_h.slice(:order)[:order]))
-  end
-
   def initialize_order
     @order = OrderForm.new
     @order.billing = fetch_or_create_address('billing')
     @order.shipping = fetch_or_create_address('shipping')
-    # session[:order] = @order
   end
 
   def fetch_or_create_address(type)
