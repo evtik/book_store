9626ff0f8e08a61a846f13c225258ba570e7a410
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2017-06-13 18:18:16 +0300

    Add admin dashboard content: orders, reviews

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,34 +1,34 @@
 ActiveAdmin.register_page "Dashboard" do
+  menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
-  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
-
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
+  content title: proc { I18n.t('active_admin.dashboard') } do
     columns do
       column do
-        panel "Recent Posts" do
-          # ul do
-            # Post.recent(5).map do |post|
-              # li link_to(post.title, admin_post_path(post))
-            # end
-          # end
-        end
-      end
-
-      column do
-        panel "Info" do
-          # para "Welcome to ActiveAdmin."
+        panel 'Recent orders' do
+          table_for Order.order('id desc').limit(10) do
+            column('Order') { |order| order.decorate.number }
+            state_column :state
+            column 'Date', :created_at
+            column 'Customer', :user, sortable: :user_id
+            column('Total') do |order|
+              number_to_currency order.subtotal + order.shipment.price
+            end
+          end
         end
       end
     end
 
-  end # content
+    columns do
+      column do
+        panel 'Recent reviews' do
+          table_for Review.order('id desc').limit(10) do
+            column('Book') { |review| review.book.title }
+            column 'Date', :created_at
+            column('User') { |review| review.user.email }
+            state_column :state
+          end
+        end
+      end
+    end
+  end
 end
--- app/admin/order.rb
+++ app/admin/order.rb
@@ -38,12 +38,4 @@ ActiveAdmin.register Order do
       end
     end
   end
-
-  form do |f|
-    f.input :aasm_read_state, label: 'Change state', as: :select,
-      collection: f.object.aasm.events(permitted: true).map(&:name)
-    f.actions
-  end
-
-  permit_params :aasm_read_state
 end
