c917f907656111156043395afc0bbc0eae957a4a
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2017-07-27 17:51:19 +0300

    Fix Heroku Devise secret key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,6 +3,8 @@ Devise.setup do |config|
 
   require 'devise/orm/active_record'
 
+  config.secret_key = ENV['DEVISE_SECRET_KEY'] if Rails.env.production?
+
   config.case_insensitive_keys = [:email]
 
   config.strip_whitespace_keys = [:email]
