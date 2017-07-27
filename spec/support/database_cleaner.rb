5260edc2ab08b10ec8cc3fb608e4c69e6fd47fe1
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2017-07-27 16:55:23 +0300

    Remove commented lines in DatabaseCleaner config

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,5 +1,4 @@
 RSpec.configure do |config|
-
   config.use_transactional_fixtures = false
 
   config.before(:suite) do
@@ -19,19 +18,13 @@ RSpec.configure do |config|
   end
 
   config.before(:each) do
-    # DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.strategy = :truncation
   end
 
   config.before(:each, type: :feature) do
-    # :rack_test driver's Rack app under test shares database connection
-    # with the specs, so continue to use transaction strategy for speed.
     driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
 
     if !driver_shares_db_connection_with_specs
-      # Driver is probably for an external browser with an app
-      # under test that does *not* share a database connection with the
-      # specs, so use truncation strategy.
       DatabaseCleaner.strategy = :truncation
     end
   end
@@ -43,5 +36,4 @@ RSpec.configure do |config|
   config.append_after(:each) do
     DatabaseCleaner.clean
   end
-
 end
