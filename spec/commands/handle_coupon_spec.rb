830e1816fceaaa11c960f7f0c353f7ddefec6285
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2017-10-09 15:19:56 +0300

    Add HandleCoupon command spec

--- spec/commands/handle_coupon_spec.rb
+++ spec/commands/handle_coupon_spec.rb
@@ -1,8 +1,30 @@
 describe HandleCoupon do
   describe '#call' do
-    it "returns 'does not exist' message with non-existent coupon code" do
-      command = described_class.new('goo')
-      expect { command.call }.to publish(:invalid, t('coupon.non_existent'))
+    context 'with invalid inputs' do
+      it "returns 'does not exist' message with non-existent coupon code" do
+        command = described_class.new('goo')
+        expect { command.call }.to publish(:invalid, t('coupon.non_existent'))
+      end
+
+      it "returns 'expired' message with expired coupon code" do
+        create(:coupon, expires: Date.today - 1.day)
+        command = described_class.new('123456')
+        expect { command.call }.to publish(:invalid, t('coupon.expired'))
+      end
+
+      it "returns 'taken' message with coupon been already taken" do
+        create(:coupon_with_order)
+        command = described_class.new('123456')
+        expect { command.call }.to publish(:invalid, t('coupon.taken'))
+      end
+    end
+
+    context 'with valid input' do
+      it 'returns coupon instance' do
+        coupon = create(:coupon)
+        command = described_class.new('123456')
+        expect { command.call }.to publish(:valid, coupon)
+      end
     end
   end
 end
