d9c3416696856ac29757000f31cbe9ab283ab2ac
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2017-10-18 14:42:41 +0300

    Move CreateOrderItemsFromCart from Rectify Command to service

---
+++ app/services/common/create_order_items_from_cart.rb
@@ -0,0 +1,12 @@
+module Common
+  class CreateOrderItemsFromCart < BaseService
+    def call(cart)
+      cart.map do |book_id, quantity|
+        OrderItem.new do |order_item|
+          order_item.book_id = book_id
+          order_item.quantity = quantity.to_i
+        end
+      end
+    end
+  end
+end
--- spec/services/common/create_order_items_from_cart_spec.rb
+++ spec/services/common/create_order_items_from_cart_spec.rb
@@ -1,9 +1,9 @@
-describe CreateOrderItemsFromCart do
+describe Common::CreateOrderItemsFromCart do
   describe '#call' do
     it 'returns array of OrderItems corresponding to cart items' do
       create_list(:book_with_authors_and_materials, 3)
       cart = { 1 => 4, 2 => 8, 3 => 12 }
-      described_class.new(cart).call.each.with_index do |item, index|
+      described_class.call(cart).each.with_index do |item, index|
         expect(item).to be_instance_of(OrderItem)
         expect(item.book_id).to eq(index + 1)
         expect(item.quantity).to eq((index + 1) * 4)
