41e1acffb54a0d9ccca2b64bbed3631724550b18
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2017-11-12 10:31:05 +0200

    Fix InitializeOrder service to get user_id from session

--- app/services/checkout/initialize_order.rb
+++ app/services/checkout/initialize_order.rb
@@ -1,18 +1,17 @@
 module Checkout
   class InitializeOrder < BaseService
     def self.build
-      new(Common::GetUserIdFromSession.build, Common::GetOrCreateAddress.build)
+      new(Common::GetOrCreateAddress.build)
     end
 
-    def initialize(*args)
-      @get_user_id, @get_or_create_address = args
+    def initialize(get_or_create_address)
+      @get_or_create_address = get_or_create_address
     end
 
     def call(session)
-      user_id = @get_user_id.call(session)
       OrderForm.new(
-        billing: @get_or_create_address.call(user_id, 'billing'),
-        shipping: @get_or_create_address.call(user_id, 'shipping'),
+        billing: @get_or_create_address.call(session, 'billing'),
+        shipping: @get_or_create_address.call(session, 'shipping'),
         items_total: session[:items_total],
         subtotal: session[:order_subtotal]
       ).tap { |order| session[:order] = order }
