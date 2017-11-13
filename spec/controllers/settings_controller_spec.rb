ab2a5304da617bcea6e7c18c25d0e394fc818211
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2017-11-13 23:31:07 +0200

    Add Addresses controller spec

--- spec/controllers/settings_controller_spec.rb
+++ spec/controllers/settings_controller_spec.rb
@@ -1,10 +1,10 @@
 describe SettingsController do
-  context 'logged in user' do
-    let(:user) { create(:user) }
+  describe 'GET show' do
+    context 'with logged in user' do
+      let(:user) { create(:user) }
 
-    before { sign_in(user) }
+      before { sign_in(user) }
 
-    context 'GET show' do
       before { get :show, params: { id: user } }
 
       it 'renders :show template' do
@@ -16,10 +16,8 @@ describe SettingsController do
         expect(assigns(:shipping)).to be_truthy
       end
     end
-  end
 
-  context 'guest user' do
-    describe 'GET show' do
+    context 'with guest user' do
       it 'redirects to login page' do
         get :show, params: { id: 1 }
         expect(response).to redirect_to(new_user_session_path)
