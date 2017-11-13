8c2c53808dfbc009cf6df66168e5b0392051825d
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2017-11-13 10:35:48 +0200

    Add FindOrInitializeAddress service spec

--- app/commands/settings/update_address.rb
+++ app/commands/settings/update_address.rb
@@ -8,20 +8,16 @@ module Settings
     end
 
     def initialize(*args)
-      @find_init_address, @address_updated_message = args
+      @find_init_address, @updated_message = args
     end
 
     def call(params, user_id)
       address_type = params.key(t('settings.show.save'))
       address = AddressForm.from_params(params[:address][address_type])
       return publish(:invalid, address) if address.invalid?
-      address = @find_init_address.call(address.id, address.to_h,
-                                        user_id, address_type)
-      if address.save
-        publish(:ok, @address_updated_message.call(address_type))
-      else
-        publish(:error, address.errors.full_messages.first)
-      end
+      address = @find_init_address.call(address.to_h, user_id)
+      return publish(:ok, @updated_message.call(address_type)) if address.save
+      publish(:error, address.errors.full_messages.first)
     end
   end
 end
--- app/services/settings/find_or_initialize_address.rb
+++ app/services/settings/find_or_initialize_address.rb
@@ -1,10 +1,9 @@
 module Settings
   class FindOrInitializeAddress < BaseService
-    def call(id, attributes, user_id, address_type)
-      Address.find_or_initialize_by(id: id).tap do |address|
+    def call(attributes, user_id)
+      Address.find_or_initialize_by(id: attributes[:id]).tap do |address|
         address.attributes = attributes
         address.user_id ||= user_id
-        address.address_type = address_type if address.address_type.blank?
       end
     end
   end
