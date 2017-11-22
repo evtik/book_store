f0df7d72c0e33ba7a4e7c7d65ef7336dedb69a6e
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2017-11-22 12:31:58 +0200

    Move strings for active_admin user config to i18n

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,16 +1,4 @@
 ActiveAdmin.register User do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
   permit_params :email, :password, :password_confirmation, :admin
 
   index do
@@ -22,7 +10,7 @@ ActiveAdmin.register User do
   filter :email
 
   form do |f|
-    f.inputs 'User details' do
+    f.inputs t('.user.user_details') do
       f.input :email
       f.input :password
       f.input :password_confirmation
