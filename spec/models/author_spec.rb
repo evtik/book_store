76b0636e92acdb90232de17890f2f618a27be7f1
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2017-11-27 22:14:23 +0200

    Move Author validation tests to AuthorForm spec

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,16 +2,4 @@ describe Author, type: :model do
   context 'association' do
     it { is_expected.to have_and_belong_to_many(:books) }
   end
-
-  context 'first name' do
-    include_examples 'name', :first_name
-  end
-
-  context 'last name' do
-    include_examples 'name', :last_name
-  end
-
-  context 'description' do
-    include_examples 'description', :description
-  end
 end
