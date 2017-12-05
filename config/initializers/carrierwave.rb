e4e2e45ef5a27ac0502fa3a229b48d4a157af875
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2017-12-05 10:56:09 +0200

    Remove mailer previwer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -26,23 +26,3 @@ CarrierWave.configure do |config|
     }
   end
 end
-# CarrierWave.configure do |config|
-  # config.cache_dir = "#{Rails.root}/tmp/uploads"
-  # config.fog_provider = 'fog/aws'
-  # config.fog_credentials = {
-    # provider: 'AWS',
-    # aws_access_key_id: 'AKIAJHFZHHFXAN3ECAFQ',
-    # aws_secret_access_key: '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',
-    # region: 'eu-central-1',
-    # endpoint: 'https://s3.eu-central-1.amazonaws.com'
-  # }
-  # config.fog_directory = 'sybookstore'
-# end
-
-# if Rails.env.test?
-  # CarrierWave.configure do |config|
-    # config.storage = :file
-    # config.enable_processing = false
-  # end
-# end
-
