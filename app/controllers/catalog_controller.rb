85cc91bb4a5605b7b253502f861d1d31e080433d
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2017-12-05 11:42:37 +0200

    Split index action of CatalogController

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -4,19 +4,24 @@ class CatalogController < ApplicationController
   def index
     present CatalogPresenter.new(params: catalog_params,
                                  categories: @categories)
-    @books = BookDecorator.decorate_collection(
-      (CategoryBooks.new(catalog_params[:category]) |
-        BooksSorter.new({
-          'sort_by' => 'created_at',
-          'order' => 'desc',
-          'limit' => 12
-        }.merge(catalog_params.to_h)))
-    )
+    @books = BookDecorator.decorate_collection(category_books | sorted)
   end
 
   private
 
   def catalog_params
     params.permit(:category, :sort_by, :order, :limit)
+  end
+
+  def category_books
+    CategoryBooks.new(catalog_params[:category])
+  end
+
+  def sorted
+    BooksSorter.new({
+      'sort_by' => 'created_at',
+      'order' => 'desc',
+      'limit' => 12
+    }.merge(catalog_params.to_h))
   end
 end
