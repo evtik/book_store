f544683360e5c77507655b3837185acda803ed98
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-01-04 18:12:21 +0200

    Add I18n support to NotifierMailer

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -3,16 +3,13 @@ class NotifierMailer < ApplicationMailer
 
   def user_email(user)
     @user = user
-    mail(to: @user.email,
-         subject: "Created account for #{@user.email}")
+    mail(to: @user.email, subject: default_i18n_subject(email: @user.email))
   end
 
   def order_email(order)
     @user = order.user
     @order = order.decorate
     @order_items = @order.order_items
-    @url = 'http://some.where'
-    mail(to: @user.email,
-         subject: "Order #{@order.number} has been successfully placed!")
+    mail(to: @user.email, subject: default_i18n_subject(number: @order.number))
   end
 end
