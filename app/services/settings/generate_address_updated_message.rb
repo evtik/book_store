f5ff537762abf0fb9b1b28e9d42e877decc19053
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-01-09 11:56:29 +0200

    Fix lost I18n in GenerateAddressUpdatedMessage service

--- app/services/settings/generate_address_updated_message.rb
+++ app/services/settings/generate_address_updated_message.rb
@@ -1,8 +1,10 @@
 module Settings
   class GenerateAddressUpdatedMessage < BaseService
+    include AbstractController::Translation
+
     def call(type)
       t('settings.show.address_saved',
-        address_type: I18n.t("checkout.address.#{type}"))
+        address_type: t("checkout.address.#{type}"))
     end
   end
 end
